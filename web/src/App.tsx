0fd35b783546bc116cf287f870d1ffae5a6be28d
fabio-peres <fabioperesg@gmail.com>
2020-10-13 01:16:13 -0300

    Criado estrutura web inicial do projeto Happy.

---
+++ web/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface TitleProps {
+  text: string;  
+}
+
+function Title(props: TitleProps) {
+  return (
+    <h1>{props.text}</h1>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Title text="Titulo 1" />
+      <Title text="Titulo 2" />
+      <Title text="Titulo 3" />
+      <Title text="Titulo 4" />
+    </div>
+  );
+}
+
+export default App;
