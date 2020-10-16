7febcfe898eaba03c44c03a71208d935d3c4568f
fabio-peres <fabioperesg@gmail.com>
2020-10-16 04:17:24 -0300

    Tratamento de erros para cada validação não atendida

--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,9 +1,25 @@
 import { ErrorRequestHandler } from 'express';
+import { ValidationError } from 'yup';
 
-const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  console.error(err);
+interface ValidationErrors {
+  [key: string]: string[];
+}
 
-  return res.status(500).json({ message: 'Internal server error' });
+
+const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
+  if (error instanceof ValidationError) {
+    let errors: ValidationErrors = {};
+
+    error.inner.forEach(err => {
+      errors[err.path] = err.errors;
+    });
+
+    return response.status(400).json({ message: 'Validation fails', errors })
+  }
+  
+  console.error(error);
+
+  return response.status(500).json({ message: 'Internal server error' });
 };
 
 export default errorHandler;
